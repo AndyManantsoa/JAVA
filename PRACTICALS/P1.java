0aa09616317507ee3fb6b48fc17b2be078f827a5
AndyManantsoa <manantsoaadrs@gmail.com>
2024-06-23 21:32:52 +0530

    COMMIT

---
+++ PRACTICALS/P1.java
@@ -0,0 +1,7 @@
+//1. write a program to display Hello World message in console window.
+
+public class P1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ PRACTICALS/P2.java
@@ -0,0 +1,7 @@
+//2. Write a program to perform arithmetic and bitwise operations in a single source program without object creation.
+
+public class P2 {
+    public static void main(String[] args) {
+    
+    }
+}
