300e7ce5497985085b99b6ce075e4507cc8dd794
AndyManantsoa <manantsoaadrs@gmail.com>
2024-06-24 10:01:08 +0530

    commit

---
+++ PRACTICALS/P3.java
@@ -0,0 +1,5 @@
+//3. Write a program to perform arithmetic and bitwise operations by creating individual methods and classes than create an object to execute the individual methods of each operation.
+
+public class P3 {
+    
+}
