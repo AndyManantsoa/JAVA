4cef3e17683af0c30a30899f013f9a8747468816
AndyManantsoa <manantsoaadrs@gmail.com>
2024-06-29 08:54:59 +0530

    commit

---
+++ EXERCISES/ArrayExo.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ArrayExo{
+
+    public void filterOdd(int []arr){
+        for(int i=0;i<arr.length ;i++){
+            if(!(arr[i]%2==0)){
+                System.out.print(arr[i] +" ");
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of an array: ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+
+        System.out.println("Enter the elements of the array: ");
+        for(int i = 0; i < size-1; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        ArrayExo arrayExo = new ArrayExo();
+        arrayExo.filterOdd(arr);
+
+    }
+}
