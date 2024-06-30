c154418f90bc80feb522073585191b65922e298f
AndyManantsoa <manantsoaadrs@gmail.com>
2024-06-30 17:48:35 +0530

    edit

--- PRACTICALS/P5.java
+++ PRACTICALS/P5.java
@@ -4,29 +4,36 @@
 
 import java.util.Scanner;
 
-public class P5.java{
-    public void findDiscriminant(float a,float b, float c){
-        float discriminant = b*b - 4*a*c;
+public class P5{
+    public void findDiscriminant(double a,double b, double c){
+        double discriminant = b*b - 4*a*c;
             if (discriminant < 0){
                 System.out.println("There are no real solutions");
             }else if(discriminant >= 0){
-                System.out.println("");
+                System.out.println("The real solutions are: ");
+                double x1= ((-b)-Math.sqrt(discriminant))/(2*a);
+                double x2= ((-b)-Math.sqrt(discriminant))/(2*a);
+                System.out.println("  x1 = "+x1);
+                System.out.println("  x2 = "+x2);
             }
         }
 
-    }
     
     public static void main(String[] args){
-        System.out.print("Enter the value of a: ");
-        float a = sc.nextFloat();
-        System.out.print("Enter the value of b: ");
-        float b = sc.nextFloat();
-        System.out.print("Enter the value of c: ");
-        float c = sc.nextFloat();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n Enter the equation in the format of ax^2 + bx + c = 0");
 
-        float discriminant = b*b - 4*a*c;
-        if (discriminant < 0){
-            System.out.println("There are no real solutions");
-        }
+        System.out.print("Enter the value of a: ");
+        double a = sc.nextDouble();
+        System.out.print("Enter the value of b: ");
+        double b = sc.nextDouble();
+        System.out.print("Enter the value of c: ");
+        double c = sc.nextDouble();
+
+        System.out.println("\n Your equation is: " + a +"x^2 + "+ b +"x + " + c + "= 0");
+        P5 p5 = new P5();
+        p5.findDiscriminant(a,b,c);        
+
     }
 }
+
