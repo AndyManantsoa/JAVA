e4b19236b007c37a7309e39ca05f9a703790a44d
AndyManantsoa <manantsoaadrs@gmail.com>
2024-07-05 20:37:07 +0530

    commit

---
+++ PRACTICES/Circle.java
@@ -0,0 +1,27 @@
+// Create a class called "Circle" with the following attribute:
+// radius (double)
+// Write a program that:
+// * Defines the Circle class with the mentioned attribute.
+// * Add a method to the Circle class called calculateArea that calculates the area of the circle (pi * radius^2) and returns the result.
+// * Create an object of the Circle class with a specific radius value.
+// * Call the calculateArea method on the circle object and print the calculated area.
+
+
+public class Circle {
+
+    double radius;
+
+    public Circle(double radius){
+        this.radius = radius;
+    }
+
+    public void calculateArea(){
+        double area = Math.PI * radius * radius;
+        System.out.println("The area of the circle is: " + area);
+    }
+
+    public static void main(String[] args) {
+        Circle circle = new Circle(20);
+        circle.calculateArea();
+    }
+}
