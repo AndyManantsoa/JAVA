45834a6c530ac9a4670b6fc29a3c49608fdc6b73
AndyManantsoa <manantsoaadrs@gmail.com>
2024-07-06 08:18:33 +0530

    commit

---
+++ EXERCISES/Bike.java
@@ -0,0 +1,18 @@
+//11) WAP to create a bike object of 2 different properties with default constructor
+
+
+public class Bike {
+    //instance variable;
+    String name;
+    String engineType;
+
+    Bike(){
+        System.out.println("Bike created");
+    }
+
+    public static void main(String[] args) {
+        Bike b1 = new Bike();
+        
+    }
+
+}
