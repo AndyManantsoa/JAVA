d733b5ee8d575d5b088c81f33af2df6125c536d7
AndyManantsoa <manantsoaadrs@gmail.com>
2024-07-06 08:33:50 +0530

    committed

--- EXERCISES/Bike.java
+++ EXERCISES/Bike.java
@@ -6,13 +6,15 @@ public class Bike {
     String name;
     String engineType;
 
-    public Bike(){
+    public Bike(String name, String engineType){
+        this.name = name;
+        this.engineType = engineType;
         System.out.println("Bike created");
     }
 
     public static void main(String[] args) {
-        Bike b1 = new Bike();
-        
+        Bike b1 = new Bike("Yamaha","v8");
+        System.out.println(b1.name+" "+b1.engineType);
     }
 
 }
