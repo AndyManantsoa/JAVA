4cc4b25dd2a63c6a1f5d08c9e0475ba3a62ac957
AndyManantsoa <manantsoaadrs@gmail.com>
2024-07-07 19:11:09 +0530

    COMMI

---
+++ EXERCISES/PRACTICE2/Car.java
@@ -0,0 +1,6 @@
+// 1) Create a class Car with instance variables for make, model, and year.
+//    Write constructors with and without arguments (default constructor).
+
+public class Car {
+    
+}
