1a00922f3e1146c7456762be8a22c05de7b5549a
AndyManantsoa <manantsoaadrs@gmail.com>
2024-07-08 10:21:59 +0530

    commit

--- PRACTICALS/P6.java
+++ PRACTICALS/P6.java
@@ -2,23 +2,32 @@
 //     The first 2 values in the sequence are 1, 1. 
 //     Every subsequent value is the sum of the 2 values preceding it. 
 //     Write a Java program that uses both recursive and non- recursive functions to print the nth value of the Fibonacci sequence?
-
 import java.util.Scanner;
 
 public class P6{
+
+
+    public int fibo (int n) {
+        if(n==0 || n==1){
+            return n;
+        }else {
+            return fibo(n-1)+fibo(n-2);
+        }
+    }
+
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n=sc.nextInt();
 
-        System.out.print("0 1 ");
-
         int x = 0, y = 1, sum = 0;
-        for (int i = 2; i <= n; i++) {
+        for (int i = 0; i <= n; i++) {
             sum = x + y;
             System.out.print(sum + " ");
             x = y;
             y = sum;
 
         }
+        P6 fibo = new P6();
+        System.out.println(fibo.fibo(n)+" ");
     }
 }
