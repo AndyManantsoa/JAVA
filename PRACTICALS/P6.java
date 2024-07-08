335340123d7114977dca4977d907665034fbad05
AndyManantsoa <manantsoaadrs@gmail.com>
2024-07-08 11:16:55 +0530

    commit

--- PRACTICALS/P6.java
+++ PRACTICALS/P6.java
@@ -1,35 +1,38 @@
-// 6. The Fibonacci sequence is defined by the following rule. 
-//     The first 2 values in the sequence are 1, 1. 
-//     Every subsequent value is the sum of the 2 values preceding it. 
-//     Write a Java program that uses both recursive and non- recursive functions to print the nth value of the Fibonacci sequence?
 import java.util.Scanner;
 
-public class P6{
+public class P6 {
 
-
-    public int fibo (int n) {
-        if(n==0 || n==1){
+    public int fibo(int n) {
+        if (n == 0 || n == 1) {
             return n;
-        }else {
-            return fibo(n-1) + fibo(n-2);
+        } else {
+            return fibo(n-1)+fibo(n-2);
         }
     }
 
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n=sc.nextInt();
-
-        int x = 0, y = 1, sum = 0;
-        for (int i = 0; i <= n; i++) {
-            sum = x + y;
-            System.out.print(sum + " ");
+    public int fiboNonRec(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        int x = 0, y = 1;
+        
+        for (int i = 1; i <= n; i++) {
+            int sum = x + y;
             x = y;
             y = sum;
+        }
+        return y;
+        
+    }
 
-        }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of series");
+
+        int n = sc.nextInt();
+
         P6 fibo = new P6();
-        for (int i = 0; i <= n; i++) {
-            System.out.println(fibo.fibo(n)+" ");
-        }
+        System.out.println("Non-recursive Fibonacci series:"+fibo.fiboNonRec(n - 1));
+        System.out.println("Recursive Fibonacci number: " + fibo.fibo(n));
     }
 }
