f99637368348c9201f8c9bf6c83e210bcab39537
Manantsoa <154403246+AndyManantsoa@users.noreply.github.com>
2024-07-08 07:56:26 +0530

    Update

--- EXERCISES/PRACTICE2/Car.java
+++ EXERCISES/PRACTICE2/Car.java
@@ -2,5 +2,32 @@
 //    Write constructors with and without arguments (default constructor).
 
 public class Car {
-    
+    String make;
+    String model;
+    int year;
+
+    //default constructor;
+    public Car(){
+        System.out.println("Car created");
+    }
+
+    //Parametrized constructor;
+    public Car(String make, String model, int year){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    public void display(){
+        System.out.println("make : "+this.make+"\n"+
+                          "model : "+this.model+"\n"+
+                          "year : "+this.year+"\n\n");
+    }
+
+    public void main(String[] args){
+        Car car1 = new Car();
+        Car car2 = new Car("Lambo", "Urus", 2019);
+        car2.display();
+    }
+
 }
