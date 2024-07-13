46ca4a04aabaac4fdf3213a7bf8a615d2e1fa8c9
AndyManantsoa <manantsoaadrs@gmail.com>
2024-07-13 09:06:10 +0530

    commit

---
+++ PROJECT/PROJECT1/Bank.java
@@ -0,0 +1,31 @@
+
+public class Bank {
+
+    //creating account 
+    String name;
+    String email;
+    int phone;
+    int balance;
+    String accountType;
+
+    // public Bank (String name, String email, int phone, int balance, String accountType){
+    //     this.name = name;
+    //     this.email = email;
+    //     this.phone = phone;
+    //     this.balance = balance;
+    //     this.accountType = accountType;
+    // }
+
+    //generate
+    int accountNumber;
+    int ifscCode;
+    static String branchName="New York City";
+
+    protected void createAccount(){
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
