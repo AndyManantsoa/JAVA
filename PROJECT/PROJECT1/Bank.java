92c64aea1117ea8bdc1c1c71d5b21aa27c731ba9
AndyManantsoa <manantsoaadrs@gmail.com>
2024-07-13 09:07:12 +0530

    commit

--- PROJECT/PROJECT1/Bank.java
+++ PROJECT/PROJECT1/Bank.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Bank {
 
@@ -26,6 +27,6 @@ public class Bank {
     }
 
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
     }
 }
