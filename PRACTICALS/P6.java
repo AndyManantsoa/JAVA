d963f03b0592a8ef402713fdf6e4821998960546
andymanantsoa <manantsoaadrs@gmail.com>
2024-09-01 20:44:04 +0530

    changes

--- PRACTICALS/P6.java
+++ PRACTICALS/P6.java
@@ -1,3 +1,9 @@
+// 6. The Fibonacci sequence is defined by the following rule. 
+//     The first 2 values in the sequence are 1, 1. 
+//     Every subsequent value is the sum of the 2 values preceding it. 
+//     Write a Java program that uses both recursive and non- recursive functions to print the nth value of the Fibonacci sequence?
+
+
 import java.util.Scanner;
 
 public class P6 {
