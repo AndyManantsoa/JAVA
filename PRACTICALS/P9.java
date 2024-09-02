e6095737ad42271a2698b4fba4750a31d36a7dde
andymanantsoa <manantsoaadrs@gmail.com>
2024-09-02 11:06:10 +0530

    edit

--- PRACTICALS/P9.java
+++ PRACTICALS/P9.java
@@ -6,11 +6,9 @@ public class P9 {
     public static void main(String[] args) {
 
         List<String> names = new ArrayList<>();
-        names.add("John");
-        names.add("Alice");
-        names.add("Bob");
-        names.add("Eve");
-        names.add("Charlie");
+        names.add("Mana");
+        names.add("Alex");
+        names.add("Coco");
 
         System.out.println("Unsorted list: " + names);
         Collections.sort(names);
@@ -18,3 +16,31 @@ public class P9 {
     }
 }
 
+
+
+/*public class P9a {
+    public static void main(String[] args) {
+        String[] arr = new String[3];
+        arr = new String[] {"Mom","Dad","Me"};
+
+        System.out.print("Unsorted: ");
+        for(int i =0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+        for(int i =0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                String temp = arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }  
+        }
+        System.out.println();
+
+        System.out.print("Sorted: ");
+        for(int i =0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+    }
+}*/
