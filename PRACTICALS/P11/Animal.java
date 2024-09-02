f6a3b602fbe154bb3857afe437d32a6476376493
andymanantsoa <manantsoaadrs@gmail.com>
2024-09-02 10:29:04 +0530

    commit

--- PRACTICALS/P11/Animal.java
+++ PRACTICALS/P11/Animal.java
@@ -1,5 +1,5 @@
-package P11;
 //11. Write a java program to represent Abstract class with example.
+package P11;
 
 public abstract class Animal {
     public abstract void sound();
--- PRACTICALS/P11/Dog.java
+++ PRACTICALS/P11/Dog.java
@@ -11,3 +11,4 @@ public class Dog extends Animal {
         dog.sound();
     }
 }
+
