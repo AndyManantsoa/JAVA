193b9519206ccc259d44986d4275afa2bc48988a
andymanantsoa <manantsoaadrs@gmail.com>
2024-09-19 21:10:56 +0530

    commit

--- PRACTICALS/P11/Dog.java
+++ PRACTICALS/P11/Dog.java
@@ -1,9 +1,19 @@
 package P11;
 
-public class Dog extends Animal {
+interface Animal {
+    public void display();
+    public abstract void sound();
+}
+
+
+public class Dog implements Animal {
     @Override
     public void sound() {
         System.out.println("The dog barks.");
+    }
+    @Override
+    public void display() {
+        System.out.println("This is a dog.");
     }
 
     public static void main(String[] args) {
