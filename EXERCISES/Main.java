48d2b6f69e7e8bbf1dd8b96ec70a21c4020eb846
andymanantsoa <manantsoaadrs@gmail.com>
2024-09-21 09:15:25 +0530

    changes

---
+++ EXERCISES/Main.java
@@ -0,0 +1,35 @@
+package EXERCISES;
+
+import java.util.*;
+
+public class Main {
+    public static boolean isEven(int a){
+        return a % 2 == 0;
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the line of numbers: ");
+        String numbers = sc.next();
+
+        StringTokenizer tokenizer = new StringTokenizer(numbers);
+
+        int sum = 0, odd = 0, even = 0;
+
+        while(tokenizer.hasMoreTokens()){
+            System.out.println(tokenizer.nextToken());
+
+            String token = tokenizer.nextToken();
+            int num = Integer.parseInt(token);
+            sum += num;
+            if(isEven(num)){
+                even++;
+            } else {
+                odd++;
+            }
+        }
+        System.out.println("The sum is: " + sum);
+        System.out.println("Number of even numbers: " + even);
+        System.out.println("Number of odd numbers: " + odd);
+    }
+}
