2cf7ffff609c2540a5547e5699cef383827ba91d
andymanantsoa <manantsoaadrs@gmail.com>
2024-10-20 15:33:04 +0530

    updates

--- PRACTICALS/P9.java
+++ PRACTICALS/P9.java
@@ -2,39 +2,32 @@
 
 import java.util.*;
 
+// public class P9 {
+//     public static void main(String[] args) {
+
+//         List<String> names = new ArrayList<>();
+//         names.add("Mana");
+//         names.add("Alex");
+//         names.add("Coco");
+
+//         System.out.println("Unsorted list: " + names);
+//         Collections.sort(names);
+//         System.out.println("Sorted list: " + names);
+//     }
+// }
+
+
 public class P9 {
     public static void main(String[] args) {
-
-        List<String> names = new ArrayList<>();
-        names.add("Mana");
-        names.add("Alex");
-        names.add("Coco");
-
-        System.out.println("Unsorted list: " + names);
-        Collections.sort(names);
-        System.out.println("Sorted list: " + names);
-    }
-}
-
-/*
-public class P9 {
-    public static void main(String[] args) {
-        String[] arr = new String[3];
-        arr = new String[] {"Mom","Dad","Me"};
+        String[] arr = {"Mom","Dad","Me"};
 
         System.out.print("Unsorted: ");
         for(int i =0;i<arr.length;i++){
             System.out.print(arr[i]+" ");
         }
 
-        for(int i =0;i<arr.length;i++){
-            for(int j=i+1;j<arr.length;j++){
-                String temp = arr[i];
-                arr[i]=arr[j];
-                arr[j]=temp;
-            }  
-        }
         System.out.println();
+        Arrays.sort(arr);
 
         System.out.print("Sorted: ");
         for(int i =0;i<arr.length;i++){
@@ -43,4 +36,4 @@ public class P9 {
 
     }
 }
-*/
+
